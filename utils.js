27d0fc06fe00feb8cdcfe9fe71cbd8228bc46160
godsenal <tmqps78@gmail.com>
2022-08-01 18:06:51 +0900

    난이도별 랜덤 아이템 추가

--- utils.js
+++ utils.js
@@ -1,13 +1,16 @@
-import fs from 'fs';
-import chalk, { Chalk } from 'chalk';
+import fs from "fs";
+import chalk from "chalk";
+import lodash from "lodash";
 
-const readFileSync = (path) => fs.readFileSync(path, { encoding: 'utf-8' });
+const { groupBy, mapValues, sampleSize } = lodash;
+
+const readFileSync = (path) => fs.readFileSync(path, { encoding: "utf-8" });
 
 export const getOrCreate = (path) => {
   try {
     return readFileSync(path);
   } catch (e) {
-    fs.writeFileSync(path, '[]');
+    fs.writeFileSync(path, "[]");
     return readFileSync(path);
   }
 };
@@ -19,27 +22,42 @@ export const writeSolvedFiles = (path, prevSolvedArray, currSolvedArray) => {
   );
 };
 
+const DifficultyColors = {
+  warm: "008080",
+  easy: "7aad0c",
+  medium: "d9901a",
+  hard: "de3d37",
+  extreme: "b11b8d",
+};
+
 export const findRandomExtinctItems = (items, count) => {
   const targets = new Set();
-  while (targets.size < count) {
-    const value = items[Math.floor(Math.random() * items.length)];
-    targets.has(value) || targets.add(value);
+  const levelMap = mapValues(
+    groupBy(items, (item) => item.split("-")[1]),
+    (items) => sampleSize(items, count)
+  );
+  const levels = Object.keys(DifficultyColors);
+
+  let levelIdx = 0;
+
+  while (
+    targets.size < count &&
+    Object.values(levelMap).some((v) => v.length)
+  ) {
+    const levelQuestions = levelMap[levels[levelIdx]];
+
+    levelQuestions?.length > 0 && targets.add(levelQuestions.shift());
+
+    levelIdx = (levelIdx + 1) % levels.length;
   }
+
   return Array.from(targets);
 };
 
 const getQuestionUrl = (id) => `https://tsch.js.org/${id}/play/ko`;
 
-const DifficultyColors = {
-  warm: '008080',
-  easy: '7aad0c',
-  medium: 'd9901a',
-  hard: 'de3d37',
-  extreme: 'b11b8d',
-};
-
 export const printQuestion = (question) => {
-  const [id, difficulty, name] = question.split('-');
+  const [id, difficulty, name] = question.split("-");
 
   console.log(
     `${chalk.bgHex(DifficultyColors[difficulty])(
