3413f95823f376e9cbe539f4a8fb77a09fae0696
godsenal <tmqps78@gmail.com>
2022-08-15 20:22:33 +0900

    :wrench: issue 에 assignee 추가

--- ci.js
+++ ci.js
@@ -3,10 +3,12 @@ import { getQuestionUrl } from "./utils.js";
 
 export const createIssue = async ({ github, context }) => {
   const questions = await main();
+  const assignees = ["Godsenal", "Jonir227"];
 
   return github.rest.issues.create({
     owner: context.repo.owner,
     repo: context.repo.repo,
+    assignees,
     title: `${new Date().toISOString().split("T")[0]} 문제`,
     body: `
 # 오늘의 문제
